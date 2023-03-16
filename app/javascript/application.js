0f9dfb2fadf3c88e325bbca264ef6f7cef284465
joevalook <joevalookaran@hotmail.com>
2023-03-16 19:51:24 -0400

    created forms

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery3
+//= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
